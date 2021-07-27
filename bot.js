9844d31098c96904bb1ec3cdf64c4a14ba990c24
Jazzy <41112271+jazzyd6099@users.noreply.github.com>
2021-07-26 20:52:52 -0400

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,15 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on("ready", () => {
+  console.log("pees");
+  
+});
+
+client.on("message", (message) => {
+  if (message.content.startsWith("ping")) {
+    message.channel.send("pong");
+  }
+});
+
+client.login("BOT_TOKEN");
