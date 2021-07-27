d043fb7fcb3e2a87edea60108436bb38593802dd
Jazzy <41112271+jazzyd6099@users.noreply.github.com>
2021-07-26 21:00:15 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,27 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
+const prefix = "$";
 
 client.on("ready", () => {
   console.log("pees");
   
+  client.user.setActivity("Thinking about Lux"); 
+       client.user.setPresence({ activity: { name: 'Thinking about Lux' }, status: 'idle' })
+  .then(console.log)
+  .catch(console.error);
+  
 });
 
 client.on("message", (message) => {
-  if (message.content.startsWith("ping")) {
+  
+  if (!message.content.startsWith(prefix)) return;
+  
+  
+  if (message.content.startsWith(prefix + "ping")) {
     message.channel.send("pong");
+  } else
+    if (message.content.startsWith(prefix + "pees")) {
+        message.channel.send("pee");
   }
 });
 
