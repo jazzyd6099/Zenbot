7f80d8e933bbdc732f3d73bffadf8ee25d058ee5
Jazzy <41112271+jazzyd6099@users.noreply.github.com>
2021-07-26 21:21:55 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,21 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const prefix = "$";
+const { prefix } = require('./config.json');
 const activities = require('./jsons/activity');
-
-//embedColors
-
-const embedRed = 0xff0000
-const embedOrange = 0xff790c
-const embedYellow = 0xffff00
-const embedGreen = 0x00ff00
-const embedBlue = 0x0064ff
-const embedPurple = 0x6a00b0
-const embedMagenta = 0x9600ff
-const embedPink = 0xff00ff
-const embedBlack = 0x000000
-const embedWhite = 0xffffff
-const embedGray = 0x777777
 
 var x = [
     1,
