6951fad01e5b2a47c00b07743b469d2bf5805303
Jazzy <41112271+jazzyd6099@users.noreply.github.com>
2021-07-26 21:06:27 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,6 +2,30 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const prefix = "$";
 
+//embedColors
+
+const embedRed = 0xff0000
+const embedOrange = 0xff790c
+const embedYellow = 0xffff00
+const embedGreen = 0x00ff00
+const embedBlue = 0x0064ff
+const embedPurple = 0x6a00b0
+const embedMagenta = 0x9600ff
+const embedPink = 0xff00ff
+const embedBlack = 0x000000
+const embedWhite = 0xffffff
+const embedGray = 0x777777
+
+var x = [
+    1,
+    2,
+    3,
+    4,
+    5,
+]
+
+var output = x[Math.floor(Math.random()*x.length)];
+
 client.on("ready", () => {
   console.log("pees");
   
@@ -22,6 +46,21 @@ client.on("message", (message) => {
   } else
     if (message.content.startsWith(prefix + "pee")) {
         message.channel.send("pee");
+    } else
+      if (message.content.startsWith(prefix+"server")) {
+           var embed = new Discord.MessageEmbed()
+	      .setColor(3447003)
+	      .setTitle("Server Information")
+	      .setDescription(`${message.guild}'s information`)
+	      .setThumbnail(message.guild.iconURL())
+	      .addField("Server ID", message.guild.id)
+	      .addField("Owner", message.guild.owner)
+	      .addField("Member Count", `This server has ${message.guild.memberCount} members.`)
+	      .addField("Roles Count", `This server has ${message.guild.roles.cache.size} roles.`)
+	      .addField("Emojis Count", `This Server has ${message.guild.emojis.cache.size} emojis.`)
+  	      .addField("You Joined At", `${message.member.joinedAt.toDateString()}`)
+  	      .setFooter(`Server created at ${message.guild.createdAt.toDateString()}`);
+				 message.channel.send({embed})
   }
 });
 
